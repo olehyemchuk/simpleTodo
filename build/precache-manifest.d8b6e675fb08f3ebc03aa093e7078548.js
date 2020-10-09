74fc34a94ed9fb655f89410366197d0390ecf5df
Oleh <emchuk86@gmail.com>
2020-10-09 16:37:30 +0300

    build

---
+++ build/precache-manifest.d8b6e675fb08f3ebc03aa093e7078548.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d0fe6265145a495f0416db117a3da38",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9324dd83cbcc05f835e",
+    "url": "/static/css/main.9033d617.chunk.css"
+  },
+  {
+    "revision": "590ba64fb3a81605103b",
+    "url": "/static/js/2.6a0cb179.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6a0cb179.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0733ac992c78fd6064ce",
+    "url": "/static/js/3.223f4516.chunk.js"
+  },
+  {
+    "revision": "d9324dd83cbcc05f835e",
+    "url": "/static/js/main.aaab0654.chunk.js"
+  },
+  {
+    "revision": "cabfaa9ebb735b0e5886",
+    "url": "/static/js/runtime-main.78c27ecc.js"
+  }
+]);
